500730eb84074dc7dbae44c8083fe6c174a0d4a4
n-arbij <omollojibran@gmail.com>
2026-02-11 20:53:00 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './pages/Home'
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
+    </Routes>
+  )
+}
+
+export default App
